f4b5c6c182deb42b62436901d4acf2ffa7d13ac8
Tom Lee <tlee418@g.ucla.edu>
2015-08-17 22:48:29 -0700

    user can comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # get 'welcome/index'
-  resources :articles
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -16,6 +15,9 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :articles do
+    resources :comments
+  end
 
   # Example resource route with options:
   #   resources :products do
